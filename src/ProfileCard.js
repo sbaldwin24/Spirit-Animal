f65f456b4f92a796ff7256726f4c770f9b4e954b
sbaldwin24 <sterling.baldwin@gmail.com>
2018-01-14 19:53:15 -0600

    added ability to upload and delte spirit animal imgs

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -4,31 +4,84 @@ import { storage, database } from "./firebase";
 import "./ProfileCard.css";
 
 class ProfileCard extends React.Component {
-  storageRef = storage.ref("/user-images").child(this.props.uid);
-  userRef = database.ref("/users").child(this.props.uid);
+  state = {
+    uploadProgress: null
+  };
 
-  handleSubmit = e => {
+  userRef = database.ref("users").child(this.props.uid);
+  storageRef = storage.ref("user-images").child(this.props.uid);
+
+  handleFileUpload = e => {
     const file = e.target.files[0];
     const uploadTask = this.storageRef
       .child(file.name)
       .put(file, { contentType: file.type });
 
+    uploadTask.on("state_changed", snapshot => {
+      const uploadProgress =
+        snapshot.bytesTransferred / snapshot.totalBytes * 100;
+      this.setState({ uploadProgress });
+    });
+
     uploadTask.then(snapshot => {
-      this.userRef.child("photoURL").set(snapshot.downloadURL);
+      this.userRef.set({
+        imageURL: snapshot.downloadURL,
+        imageName: file.name
+      });
+      this.setState({ uploadProgress: null });
     });
   };
 
+  handleFileRemoval = () => {
+    const { imageName } = this.props.user;
+
+    this.storageRef
+      .child(imageName)
+      .delete()
+      .then(() => {
+        this.userRef.update({
+          imageURL: null,
+          imageName: null
+        });
+      });
+  };
+
   render() {
-    const { displayName, photoURL } = this.props.user;
+    const {
+      photoURL,
+      displayName,
+      email,
+      imageURL,
+      imageName
+    } = this.props.user;
+    const { uploadProgress } = this.state;
+
     return (
-      <article className="ProfileCard">
-        <img className="ProfileCard--photo" src={photoURL} />
-        <h3>{displayName}</h3>
-        <FileInput
-          accept=".png,.gif,.jpg"
-          onChange={this.handleSubmit}
-          placeholder="Select an image"
+      <article className="UserProfile">
+        {uploadProgress && (
+          <div>
+            <strong>Uploading</strong>: {Math.round(uploadProgress)}%
+          </div>
+        )}
+        <img
+          className="UserProfile--photo"
+          src={imageURL || photoURL}
+          alt={displayName}
         />
+        <div className="UserProfile--identification">
+          <h3 className="UserProfile--displayName">{displayName}</h3>
+          <p className="UserProfile--email">{email}</p>
+          <FileInput
+            name="UserProfile--fileinput"
+            accept=".png,.gif,.jpg"
+            placeholder={imageName || "Spirit Animal Image"}
+            className="UserProfile--upload"
+            onChange={this.handleFileUpload}
+          />
+          {imageName && (
+            <button onClick={this.handleFileRemoval}>Delete Image</button>
+          )}
+        </div>
       </article>
     );
   }
